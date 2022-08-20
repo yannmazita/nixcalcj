196d7a29a7d5471963cc9975d88a79c47393dbb6
Yann Mazita <dev.mazita@gmail.com>
2022-08-20 15:32:34 +0200

    Implemented StoreNumber

--- src/main/java/com/nixcalcj/calculator/Expression.java
+++ src/main/java/com/nixcalcj/calculator/Expression.java
@@ -1,4 +1,30 @@
 package com.nixcalcj.calculator;
 
 public class Expression {
+    private String infixExpr;
+    private StringBuffer postfixExpr = new StringBuffer();
+    private StringBuffer tmpNumString = new StringBuffer();
+    public boolean isIntegerOnly = true;
+    public Expression(String inputExpr){
+        infixExpr = inputExpr;
+    }
+
+    private int StoreNumber(final int pos, String type){
+        int i = pos;
+        if (type.equals("in")){
+            while (i < infixExpr.length() && (Character.isDigit(infixExpr.charAt(i)) || infixExpr.charAt(i) == '.')) {
+                if (infixExpr.charAt(i) == '.') {
+                    isIntegerOnly = false;
+                }
+                tmpNumString.append(infixExpr.charAt(i));
+                i++;
+            }
+        } else if (type.equals("post")){
+            while (i < postfixExpr.length() && (Character.isDigit(infixExpr.charAt(i)) || postfixExpr.charAt(i) == '.')){
+                tmpNumString.append(postfixExpr.charAt(i));
+                i++;
+            }
+        }
+        return i;
+    }
 }
